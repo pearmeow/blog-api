60060d172ecf3f03d8dc97f25410e84f873a159a
Perry Huang <perryhuang2015@gmail.com>
2025-08-31 17:33:07 -0400

    feat: add validators and fix fields in post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,24 +1,63 @@
 import db from "../models/index.js";
+import * as validator from "../middleware/validator.js";
 
 export const get = async (req, res) => {
     res.json(await db.readPost());
 };
 
-export const getId = async (req, res) => {
-    res.json(await db.readPost(req.params.id));
-};
+export const getId = [
+    validator.idParamFactory("postId"),
+    validator.validateResults,
+    async (req, res) => {
+        res.json(await db.readPost(Number(req.params.postId)));
+    },
+];
 
-export const post = async (req, res) => {
-    const { authorId, title, text, date, published } = req.body;
-    res.json(await db.createPost(authorId, title, text, date, published));
-};
+// TODO: validate author/admin status
+export const post = [
+    validator.textBodyFactory("title", "Title", 1, 50),
+    validator.textBodyFactory("text", "Post", 1),
+    validator.date,
+    validator.published,
+    validator.validateResults,
+    async (req, res) => {
+        // TODO: get userId from user and authorize (check author status)
+        let userId = 1;
+        const { title, text, date, published } = req.body;
+        res.json(
+            await db.createPost(Number(userId), title, text, date, published),
+        );
+    },
+];
 
-export const putId = async (req, res) => {
-    const { id, title, text, date, published } = req.body;
-    res.json(await db.updatePost(id, title, text, date, published));
-};
+// TODO: validate user author status
+export const putId = [
+    validator.idParamFactory("postId"),
+    validator.textBodyFactory("title", "Title", 1, 50),
+    validator.textBodyFactory("text", "Post", 1),
+    validator.date,
+    validator.published,
+    validator.validateResults,
+    async (req, res) => {
+        // TODO: check author status with userId and check post author to
+        // see if they match up
+        // authors can only edit their own posts
+        const { postId } = req.params;
+        const { title, text, date, published } = req.body;
+        res.json(
+            await db.updatePost(Number(postId), title, text, date, published),
+        );
+    },
+];
 
-export const delId = async (req, res) => {
-    const { id } = req.body;
-    res.json(await db.deletePost(id));
-};
+// TODO: validate user author/admin status
+export const delId = [
+    validator.idParamFactory("postId"),
+    validator.validateResults,
+    async (req, res) => {
+        // TODO: validate userId with post's author
+        // or if user is an admin
+        const { postId } = req.params;
+        res.json(await db.deletePost(Number(postId)));
+    },
+];
