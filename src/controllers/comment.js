fe40e45d224ea5f2626b1c5e65f2131e39ee9798
Perry Huang <perryhuang2015@gmail.com>
2025-08-31 17:32:51 -0400

    feat: add validators and fix up comment controller

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -1,24 +1,65 @@
 import db from "../models/index.js";
+import * as validator from "../middleware/validator.js";
 
-export const get = async (req, res) => {
-    res.json(await db.readComment());
-};
+// gets comments in a post, not all comments
+export const get = [
+    validator.idParamFactory("postId"),
+    async (req, res) => {
+        res.json(await db.readComment(null, Number(req.params.postId)));
+    },
+];
 
-export const getId = async (req, res) => {
-    res.json(await db.readComment(req.params.id));
-};
+// gets specific comment from a post
+export const getId = [
+    validator.idParamFactory("commentId"),
+    validator.validateResults,
+    async (req, res) => {
+        res.json(await db.readComment(Number(req.params.commentId)));
+    },
+];
 
-export const post = async (req, res) => {
-    const { postId, authorId, text } = req.body;
-    res.json(await db.createComment(postId, authorId, text));
-};
+// requires being at least a user
+export const post = [
+    validator.idParamFactory("postId"),
+    validator.textBodyFactory("text", "Comment", 1, 500),
+    validator.validateResults,
+    async (req, res) => {
+        // TODO: get authorid from req.user if authentication passed
+        let authorId = 1;
+        const { postId } = req.params;
+        const { text } = req.body;
+        res.json(
+            await db.createComment(Number(postId), Number(authorId), text),
+        );
+    },
+];
 
-export const putId = async (req, res) => {
-    const { id, text, date } = req.body;
-    res.json(await db.updateComment(id, text, date));
-};
+// TODO: validate user
+// requires being at least a user
+// user can only edit their own
+export const putId = [
+    validator.idParamFactory("commentId"),
+    validator.textBodyFactory("text", "Comment", 1, 500),
+    validator.date,
+    validator.validateResults,
+    async (req, res) => {
+        // TODO: validate that user is editing their own comment
+        const { commentId } = req.params;
+        const { text, date } = req.body;
+        res.json(await db.updateComment(Number(commentId), text, date));
+    },
+];
 
-export const delId = async (req, res) => {
-    const { id } = req.body;
-    res.json(await db.deleteComment(id));
-};
+// TODO: validate user
+// user can only delete their own messages
+// an admin can delete any user's messages
+export const delId = [
+    validator.idParamFactory("commentId"),
+    validator.validateResults,
+    async (req, res) => {
+        // TODO: validate that user is deleting their own comment
+        // or that user is an admin
+        const { commentId } = req.params;
+        res.json(await db.deleteComment(Number(commentId)));
+    },
+];
