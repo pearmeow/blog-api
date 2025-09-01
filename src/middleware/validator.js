2ea3e088ef5d536fb8446efd0e6ff07ccb4527e3
Perry Huang <perryhuang2015@gmail.com>
2025-09-01 12:15:07 -0400

    fix: add usage of next to validator

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -54,9 +54,10 @@ export const password = [
         .withMessage("Passwords must match"),
 ];
 
-export const validateResults = (req, res) => {
+export const validateResults = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
+    next();
 };
