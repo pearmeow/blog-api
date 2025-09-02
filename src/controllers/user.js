f19aaadd3cb22c31cbd2ac74202cba08a691d0c0
Perry Huang <perryhuang2015@gmail.com>
2025-09-02 08:27:07 -0400

    feat: rely on author for some user rest operations

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,7 +5,7 @@ import passport from "passport";
 
 export const get = [
     passport.authenticate("jwt", { session: false }),
-    validator.isAdmin,
+    validator.isAuthor,
     async (req, res) => {
         res.json(await db.readUser());
     },
@@ -13,12 +13,12 @@ export const get = [
 
 export const getId = [
     passport.authenticate("jwt", { session: false }),
-    validator.isAdmin,
+    validator.isAuthor,
     validator.idParamFactory("userId"),
     validator.validateResults,
     async (req, res) => {
         const { userId } = req.params;
-        res.json(await db.readUser(Number(userId)));
+        res.json(await db.readUserById(Number(userId)));
     },
 ];
 
@@ -36,41 +36,20 @@ export const post = [
 export const putId = [
     passport.authenticate("jwt", { session: false }),
     validator.idParamFactory("userId"),
-    validator.password.optional(),
-    validator.confirm.optional(),
-    validator.boolParamFactory("isAuthor").optional(),
-    validator.boolParamFactory("isAdmin").optional(),
+    validator.password,
+    validator.confirm,
     validator.validateResults,
     async (req, res) => {
         const { userId } = req.params;
         if (!req.body) {
             return res.end();
         }
-        const { password, confirm, isAuthor, authorCode, isAdmin, adminCode } =
-            req.body;
-        const errors = [];
-        // edge case where user types password but leaves confirm empty
-        let hashedPass = password;
-        if (password && password !== confirm) {
-            errors.push({ msg: "Passwords must match" });
-        } else if (password) {
-            hashedPass = await hashPassword(password);
-        }
-        if (isAuthor && authorCode !== process.env.AUTHOR_CODE) {
-            errors.push({ msg: "Author code is invalid" });
-        }
-        if (isAdmin && adminCode !== process.env.ADMIN_CODE) {
-            errors.push({ msg: "Admin code is invalid" });
-        }
-        if (errors.length > 0) {
-            return res.status(400).json(errors);
-        }
+        const { password } = req.body;
+        const hashedPass = await hashPassword(password);
         res.json(
             await db.updateUser({
                 id: Number(userId),
                 password: hashedPass,
-                isAuthor,
-                isAdmin,
             }),
         );
     },
@@ -82,7 +61,7 @@ export const delId = [
     validator.validateResults,
     async (req, res) => {
         const { userId } = req.params;
-        if (req.user.id === Number(userId) || req.user.isAdmin) {
+        if (req.user.id === Number(userId) || req.user.type === "author") {
             return res.json(await db.deleteUser(Number(userId)));
         }
         return res.status(401).end();
