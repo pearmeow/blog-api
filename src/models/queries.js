63cd4ca15b566c18536cae66e57000adbd5ac6a9
Perry Huang <perryhuang2015@gmail.com>
2026-02-07 17:24:15 -0500

    feat: include id with with comment in post query

--- src/models/queries.js
+++ src/models/queries.js
@@ -37,6 +37,7 @@ export const readPostById = async (id) => {
                     author: {
                         select: {
                             username: true,
+                            id: true,
                         },
                     },
                 },
